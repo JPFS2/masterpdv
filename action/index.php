43f923133afd2297ded383f54d0d374c0c0c4142
JPFS2 <johnnathanpacifico@gmail.com>
2023-05-23 09:52:38 -0300

    12

---
+++ action/index.php
@@ -0,0 +1,33 @@
+<?php 
+require 'config.php';
+
+$lista = [];
+$sql = $pdo->query("SELECT * FROM usuarios");
+
+if($sql->rowCount() ){
+    $lista = $sql->fetchAll(PDO::FETCH_ASSOC);
+}
+?>
+
+<a href="adicionar.php">ADICIONAR USUARIO</a>
+<table border='1' width='100%'>
+    <tr>
+        <th>ID</th>
+        <th>NOME</th>
+        <th>EMAIL</th>
+        <th>AÇÕES</th>
+    </tr>
+    <?php foreach($lista as $usuario): ?>
+        <tr>
+            <th><?= $usuario['id']; ?></th>
+            <th><?= $usuario['nome']; ?></th>
+            <th><?= $usuario['email']; ?></th>
+            <th>
+                <a href="editar.php?id=<?= $usuario->getId()?>">Editar</a>
+                <a href="excluir.php?id=<?= $usuario->getId()?>" onclick="return confirm('Tem certeza que deseja exclur ?')">Excluir</a>
+            </th>
+        </tr>
+    <?php endforeach; ?>
+    <tr>
+    </tr>
+</table>
